3da827d129b06617dff449d33bc78a5f5e790283
R.Oto <r.obusinesscommercial@Gmail.com>
2024-10-07 19:10:33 +0400

    creating api

--- backend/controllers/controller.js
+++ backend/controllers/controller.js
@@ -1,7 +1,13 @@
 import Book from "../models/model.js";
 
 export const getBook = async (req,res) => {
-    
+    try {
+        const books = await Book.find(); 
+        return res.status(200).json(books); 
+    } catch (error) {
+        console.error(error); 
+        return res.status(500).json({ message: "Failed to retrieve books", error });
+    }
 }
 
 export const createBook = async (req,res) => {
@@ -19,9 +25,37 @@ export const createBook = async (req,res) => {
 }
 
 export const updateBook = async (req,res) => {
+    const id = req.params.id;
+    const updateData = req.body; 
+
+try {
+    const book = await Book.findByIdAndUpdate(id, updateData, { new: true });
     
+    if (!book) {
+        return res.status(404).json({ message: "Book not found" });
+    }
+    
+    return res.status(200).json({ message: "Book updated", book });
+} catch (error) {
+    console.error(error); 
+    return res.status(500).json({ message: "Failed to update", error });
+}
+
 }
 
 export const deleteBook = async (req,res) => {
-    
+    const id = req.params.id;
+
+    try {
+        const book = await Book.findByIdAndDelete(id);
+        
+        if (!book) {
+            return res.status(404).json({ message: "Book not found" });
+        }
+
+        return res.status(200).json({ message: "Book deleted successfully" });
+    } catch (error) {
+        console.error(error); 
+        return res.status(500).json({ message: "Failed to delete book", error });
+    }
 }
