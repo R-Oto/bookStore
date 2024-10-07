8e9733fddc18353e2271a8b99b827ae17799218e
R.Oto <r.obusinesscommercial@Gmail.com>
2024-10-07 19:24:46 +0400

    api completed

--- backend/controllers/controller.js
+++ backend/controllers/controller.js
@@ -1,6 +1,6 @@
 import Book from "../models/model.js";
 
-export const getBook = async (req,res) => {
+export const getBooks = async (req,res) => {
     try {
         const books = await Book.find(); 
         return res.status(200).json(books); 
@@ -21,6 +21,23 @@ export const createBook = async (req,res) => {
     }catch(error){
         console.log(error);
         return res.status(500).json({message: "Failed to create", error});
+    }
+}
+
+export const getBook = async (req,res) => {
+    const id = req.params.id;
+
+    try {
+        const book = await Book.findById(id);
+        
+        if (!book) {
+            return res.status(404).json({ message: "Book not found" });
+        }
+
+        return res.status(200).json(book);
+    } catch (error) {
+        console.error(error); 
+        return res.status(500).json({ message: "Failed to retrieve book", error });
     }
 }
 
--- backend/routes/route.js
+++ backend/routes/route.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { createBook, deleteBook, getBook, updateBook } from "../controllers/controller.js";
+import { createBook, deleteBook, getBook, getBooks, updateBook } from "../controllers/controller.js";
 const router = express.Router();
 
-router.get("/", getBook);
+router.get("/", getBooks);
 router.post("/", createBook);
+router.get("/:id", getBook)
 router.put("/:id", updateBook);
 router.delete("/:id", deleteBook);
 
